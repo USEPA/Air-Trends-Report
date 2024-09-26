a09c5824c7ee7dff09776b1e3a852297f9688dfb
aevans04 <evans.aaron@epa.gov>
2024-09-26 11:51:41 -0400

    initial push of 2024 air trends report

---
+++ etrends_2024/src/js/MapChartResize.js
@@ -0,0 +1,41 @@
+var MapChartResize = function (host,fn,delay) {
+//Host is the map or chart object that the tooltip is attached to  
+  this.host = host;
+  this.fn = fn;
+  this.delay = delay;
+//set the resize event handler. It is a function now so we can turn resize on/off for charts in tooltips
+  this.set();
+}
+
+MapChartResize.prototype.set = function() 
+{
+  //Don't even try to set this stuff for phones
+  //Let phones resizing now to handle screen rotating
+//  if (isMobile.any) return;
+  
+//The d3 onResize hanlder needs a name space so we can have multiple onReisze handlers 
+//Then name will be the class type (function name) of the host and the dom ID of the div containing the host
+  d3.select(window).on('resize.' + this.host.constructor.name + '_' + this.host.domID, this.debounce(this.fn, this.delay)); 
+};
+
+MapChartResize.prototype.clear = function()
+{
+//clear the handler out so that we don't handle resize events in situations such as chart in tooltip with tooltip closed 
+  d3.select(window).on('resize.' + this.host.constructor.name + '_' + this.host.domID, null); 
+};
+
+MapChartResize.prototype.debounce = function(fn, delay) 
+{
+  var timeoutID = -1;
+  return function() {
+     if (timeoutID > -1) {
+        window.clearTimeout(timeoutID);
+     }
+   timeoutID = window.setTimeout(fn, delay);
+  }
+};
+
+//Set that this module is loaded for egam
+if (etrends.library.utilities) etrends.library.utilities.setModuleLoadedForEGAM('MapChartResize.js');
+
+//# sourceURL=etrends/MapChartResize.js
